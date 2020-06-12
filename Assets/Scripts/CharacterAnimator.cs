278ecb0504cb8771da9a346fbcbb7d047c3412d8
Valentyn Vasylevskyy <valentun9@gmail.com>
2020-06-12 20:12:55 +0300

    initial commit

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = .1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude /*speed*/ / agent.speed /*max speed*/;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+        Debug.Log("animator: " + animator);
+        Debug.Log("speedPercent: " + speedPercent);
+    }
+}
---
+++ Assets/Scripts/Items/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    private void PickUp()
+    {
+        Debug.Log("Picking up " + item.name);
+
+        // Add to inventory
+        bool wasPickedUp = Inventory.instance.Add(item);
+
+        if (wasPickedUp) Destroy(gameObject);
+    }
+}
